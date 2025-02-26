b38573c6b44a0547ee8f63111f19bf91f3633725
Стас <stasnocap@gmail.com>
2025-02-26 16:04:46 +0300

    complete 01.a.HttpClient of define-private-public.CSharpNetworking tutorial

---
+++ Uncategorized/define-private-public.CSharpNetworking/01.a.HttpClient/Program.cs
@@ -0,0 +1,9 @@
+﻿using var httpClient = new HttpClient();
+
+var response = await httpClient.GetAsync("https://google.com");
+
+var result = await response.Content.ReadAsByteArrayAsync();
+
+using var fileStream = File.Create("index.html");
+
+await fileStream.WriteAsync(result);
