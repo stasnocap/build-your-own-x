97851e10f60c89928cc6777d1a327dae204b61c8
Стас <stasnocap@gmail.com>
2025-02-26 16:54:56 +0300

    complete 02.HttpListener of define-private-public.CSharpNetworking tutorial

---
+++ Uncategorized/define-private-public.CSharpNetworking/02.HttpListener/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Net;
+using System.Text;
+
+var pageViews = 0;
+var page = """
+           <DOCTYPE! html>
+           <body>
+                Page views: {0}
+                <form action="shutdown" method="get">
+                    <button type="submit">Shutdown</button>
+                </form>
+           </body>
+           """;
+
+using var listener = new HttpListener();
+
+listener.Prefixes.Add("http://localhost:12345/");
+
+listener.Start();
+
+while (true)
+{
+    var context = await listener.GetContextAsync();
+
+    if (context.Request.Url?.AbsolutePath == "/shutdown")
+    {
+        break;
+    }
+
+    if (context.Request.Url?.AbsolutePath != "/favicon.ico")
+    {
+        pageViews++;
+    }
+
+    var formattedPage = string.Format(page, pageViews);
+
+    var bytes = Encoding.UTF8.GetBytes(formattedPage);
+
+    context.Response.ContentType = "text/html";
+
+    await context.Response.OutputStream.WriteAsync(bytes);
+
+    context.Response.Close();
+}
+
+listener.Close();
