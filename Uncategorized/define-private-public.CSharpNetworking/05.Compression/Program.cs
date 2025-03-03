e9d4cf9ea0dfb1f24ba983a68f90505dd485e9aa
stasnocap <stasnocap@gmail.com>
2025-03-03 11:18:58 +0300

    complete 05.Compression of define-private-public.CSharpNetworking tutorial

---
+++ Uncategorized/define-private-public.CSharpNetworking/05.Compression/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.IO.Compression;
+
+using var uncompressedFile = File.Open("original.txt", FileMode.Open);
+using var compressedFile = File.Create("compressed.gz");
+using var gZipStream = new GZipStream(compressedFile, CompressionLevel.Optimal);
+uncompressedFile.CopyTo(gZipStream);
