8b236af0d9eb4a24f8656256ee66d005f616c7d4
stasnocap <stasnocap@gmail.com>
2025-03-17 11:44:02 +0300

    complete part 11 of cstack/db_tutorial

--- Database/cstack.db_tutorial/spec/main_spec.rb
+++ Database/cstack.db_tutorial/spec/main_spec.rb
@@ -7,7 +7,11 @@ describe 'database' do
     raw_output = nil
     IO.popen("./main test.db", "r+") do |pipe|
       commands.each do |command|
-        pipe.puts command
+        begin
+          pipe.puts command
+        rescue Errno::EPIPE
+          break
+        end
       end
 
       pipe.close_write
@@ -184,7 +188,8 @@ describe 'database' do
       "    - 12",
       "    - 13",
       "    - 14",
-      "db > Need to implement searching an internal node",
+      "db > Executed.",
+      "db > ",
     ])
   end
 end
